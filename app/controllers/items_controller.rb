127d6b3bd4bf400b8c6255772410549e242d29f5
hyperwoman <hypergirl_sy@yahoo.co.jp>
2021-06-06 20:08:15 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
 
   def index
     @items = Item.all
@@ -15,12 +16,6 @@ class ItemsController < ApplicationController
     else
       render :new
     end
-  end
-  
-  def show
-  end
-
-  def edit
   end
 
   private
