e13469cee3651f82c91341e627612225dc341c89
hyperwoman <hypergirl_sy@yahoo.co.jp>
2021-06-06 18:17:17 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,32 @@
 class ItemsController < ApplicationController
+
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :category_id, :status_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :price)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,22 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 } 
+  validates :status_id, presence: true, numericality: { other_than: 1 } 
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 1 } 
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 } 
+  validates :shipping_day_id, presence: true, numericality: { other_than: 1 } 
+  validates :price,  presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :Prefecture
+  belongs_to :shipping_day
+
 end
