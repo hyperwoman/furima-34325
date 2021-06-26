7b0c3df3e29b05be27cfda889e0026a74f820dc4
hyperwoman <hypergirl_sy@yahoo.co.jp>
2021-06-26 21:57:20 +0900

    テストコードの実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,11 +3,11 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :city
     validates :house_number
-    validates :phone_number, numericality: { with: /\A\d{11}\z/ }
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
 
   end
 
