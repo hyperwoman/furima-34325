e846481e86d774af1f55446ce0b3cdd0d8a2585c
hyperwoman <hypergirl_sy@yahoo.co.jp>
2021-06-26 16:22:49 +0900

    フォームの情報を保存できるようにする

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -14,6 +14,6 @@ class OrderAddress
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
